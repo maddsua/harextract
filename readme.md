c6f47156f76739bc1b3b309219f2587e1290e840
maddsua <maddsuapro@gmail.com>
2022-05-22 21:24:12 +0300

    init

---
+++ readme.md
@@ -0,0 +1,15 @@
+## Web Archive (.har) Extractor
+
+### Version 1.0
+
+This thing does exactly what you think of it. 
+
+It extracts files from `.har` as any ordinary archiver would extract from any ordinary archive.
+
+I hate writing readmes and it does not need a wall of text.
+
+Interface:
+
+![Image](doc/interface.png)
+
+[Download package 📦](bin/unhar-1.0.0.zip)
---
+++ src/harextract_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef HAREXTRACT_PRIVATE_H
+#define HAREXTRACT_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.83"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	83
+#define COMPANY_NAME	"maddsua"
+#define FILE_VERSION	"1.0.0.83"
+#define FILE_DESCRIPTION	"Web Archive Extractor"
+#define INTERNAL_NAME	"unhar"
+#define LEGAL_COPYRIGHT	"https://gitlab.com/maddsua"
+#define LEGAL_TRADEMARKS	"2022 maddsua/dev"
+#define ORIGINAL_FILENAME	"unhar"
+#define PRODUCT_NAME	"HAR Extractor"
+#define PRODUCT_VERSION	"1.0.0.83"
+
+#endif /*HAREXTRACT_PRIVATE_H*/
